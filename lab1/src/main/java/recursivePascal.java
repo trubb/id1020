f8733625a83651b2d2aff49b92438f5f9a29a946
erpette <erpette@kth.se>
2017-08-31 17:08:10 +0200

    short work on that weird-ass pascal shit

--- lab1/src/main/java/recursivePascal.java
+++ lab1/src/main/java/recursivePascal.java
@@ -1,10 +1,46 @@
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
 
 public class recursivePascal {
 
-    public static void main(String[] args){
+    /*
+        if true print triangle right side up
+        if false print triangle upside down
+    */
 
+    // calculate (n,k) by using (n,k) == (n-1, k-1) + (n-1, k)
+    public int binom(int n, int k) {
+
+        // if n (row) is 0, or if n == k (column) the number to return will be 1 due to position within the triangle
+        if (n == 0 || n == k) {
+            return 1;
+        } else {
+            return binom(n - 1, k - 1) + binom(n - 1, k);
+        }
 
     }
 
+    public void printPascal( int n) {
+
+
+
+
+        // print pascals triangle magically
+    }
+
+    public static void main(String[] args){
+
+        // handle how many rows of pascal triangle should be printed
+
+        //ask how many rows to print
+        System.out.println("How many rows shall be printed");
+        while (!StdIn.isEmpty()) {
+            int n = StdIn.readInt();
+        }
+
+        //ask what direction to print it
+
+
+    }
 
 }
